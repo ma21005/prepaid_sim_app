68ad5083ac2d7564ddf6710fd1a85aa5d8b74884
ma21005 <hockey0513hockey@gmail.com>
2022-02-25 17:07:55 +0000

    form_ajax_ok

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,9 +7,22 @@ class HomesController < ApplicationController
         end
     end
     
+    def index_2 #クリック時に「~で使えるSIMカード」とテキストを書き換えるためのアクション
+        if params[:country]
+            @country = Country.find_by(reg_region: params[:country])
+            render json: @country
+        end
+    end
+    
+    
     def search   #検索フォームからの送信
         @country = Country.search(params[:search])
         @sims = @country.sims
         render partial: "homes/sim", collection: @sims, as: 'sim'
+    end
+    
+    def search_2 #フォーム検索時に「~で使えるSIMカード」とテキストを書き換えるためのアクション
+        @country = Country.search(params[:search])
+        render json: @country
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
    
     get '/homes/index'
+    get 'homes/index_2' => 'homes#index_2'
     get 'homes/search' => 'homes#search'
+    get 'homes/search_2' => 'homes#search_2'
     
     root to: "homes#index"
 end
