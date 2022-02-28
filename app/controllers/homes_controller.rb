31442edd0fd18faee95f3ecca403be1465ad3d06
ma21005 <hockey0513hockey@gmail.com>
2022-02-28 18:31:16 +0000

    add_country_data

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,12 +2,8 @@ class HomesController < ApplicationController
     def index
         if params[:country]
             @country = Country.find_by(reg_region: params[:country])
-            if @country.sims
-                @sims = @country.sims
-                render partial: "homes/sim", collection: @sims, as: 'sim'
-            else #対応してるSIMが無いときのパーシャル
-                render partial: "homes/not_found"
-            end
+            @sims = @country.sims
+            render partial: "homes/sim", collection: @sims, as: 'sim'
         end
     end
     
